928dc7eb690a51cd871bbe863f0fd3ee62174f27
Varun Iyer <varun_iyer@protonmail.ch>
2019-04-12 16:14:37 -0700

    Kinda forgot that

--- main.c
+++ main.c
@@ -18,6 +18,7 @@
 #include "hal.h"
 #include "log.h"
 #include "sd.h"
+#include "gps.h"
 #include "hal_fsmc_sdram.h"
 #include "is42s16400j.h"
 #include "ov5640.h"
@@ -41,6 +42,11 @@ int main(void) {
 		int err = log_image();
 		log_data();
 		if(err) LOG_ERR_LED();
+		else LOG_OK_LED();
+		chThdSleepMilliseconds(1000);
+		LOG_CLEAR_LED();
+		chThdSleepMilliseconds(1000);
+		if(gps_ping()) LOG_ERR_LED();
 		else LOG_OK_LED();
 		chThdSleepMilliseconds(1000);
 		LOG_CLEAR_LED();
