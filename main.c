861efe2817b121c37d744c153ac81e90c7402766
Varun Iyer <varun_iyer@protonmail.com>
2019-08-12 11:45:00 -0700

    Updated README

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2013-2015 Andrea Zoppi
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 #include "ch.h"
 #include "hal.h"
 #include "log.h"
@@ -18,15 +34,26 @@ int main(void) {
 	int init_err = 0;
 	halInit();
 	chSysInit();
-	log_init();
+	init_err &= log_init();
 	init_err &= gps_init();
 	pi2cInit();
 
 	while (true) {
 		log_data();
-		LED_OK();
+		 
+		char log[MAX_LOG_LEN];
+		char* ptr = log;
+		ptr += snprintf(ptr, log + MAX_LOG_LEN - ptr, "I2C Scan:");
+		uint8_t addresses[127];
+		uint8_t numd = I2C_scan(addresses);
+		for(int i = 0; i < numd; i++) {
+			ptr += snprintf(ptr, log + MAX_LOG_LEN - ptr, " %02X", addresses[i]);
+		}
+		log_message(log, LOG_VERBOSE);
+		 
+		LOG_OK_LED();
 		chThdSleepMilliseconds(1000);
-		LED_CLEAR();
+		LOG_CLEAR_LED();
 		chThdSleepMilliseconds(1000);
 	}
 }
