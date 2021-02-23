405dc242442fe77f67c0c3aa02ee932147efa34c
jsteng19 <jstenger@ucsb.edu>
2021-02-22 18:41:13 -0800

    bug fix: added base case for image naming

--- src/camera.py
+++ src/camera.py
@@ -3,8 +3,11 @@ import os
 def take_image(dir):
     #stub, only performs logging
     images = os.listdir(dir)
-    prev_img_name = max(images)
-    prev_img_num = int(prev_img_name[3:])
+    if images:
+        prev_img_name = max(images)
+        prev_img_num = int(prev_img_name[3:])
+    else:  # there are no images
+        prev_img_num = -1
     img_name = dir + "/img" + str(prev_img_num + 1) + ".jpg"
 
     return img_name
