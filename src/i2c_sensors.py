b496e6c3d3eb4836364560fc543be2ff9fb986be
jsteng19 <jstenger@ucsb.edu>
2021-02-22 16:36:35 -0800

    sensors.py is now i2c_sensors.py: a wrapper that initializes sensors on a single i2c bus and returns an object that exposes their .read() functs

---
+++ src/i2c_sensors.py
@@ -0,0 +1,31 @@
+# https://docs.micropython.org/en/latest/library/machine.I2C.html
+from machine import I2C
+import LTR329ALS01, TMP100
+
+sensor_addresses = {'BME280': 0x77, 'TMP100_1': 0x48, 'TMP100_2': 0x49, 'BMX160': 0x68, 'LTR_329ALS_01': 0x29}
+
+
+class Sensors:
+    def __init__(self, bus=2):
+        i2c = I2C(bus)          # create I2C peripheral at frequency of 400kHz
+        self.tmp2 = TMP100(i2c, sensor_addresses['TMP100_2'])
+        self.ltr = LTR329ALS01(i2c)
+
+    # def get_humidity_data(self):  # stub
+    #     pass
+    #
+    # def get_temp1_data(self):  # stub
+    #     pass
+    #     # if(i2c.is_ready())
+    #
+    # def get_temp2_data(self):  # stub
+    #     tmp = TMP100(self.i2c, sensor_addresses['TMP100_2'])
+    #
+    # def get_orientation_data(self):  # stub
+    #     pass
+    #
+    # def get_light_data(self):  # stub
+    #     ltr = LTR329ALS01(self.i2c)
+    #     return ltr.light()
+
+
